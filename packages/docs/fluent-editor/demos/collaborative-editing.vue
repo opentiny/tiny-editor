9a96b1c6421e607902a329f5c04d77b0bce2f0eb
yinlin124 <yinlin124@gmail.com>
2025-08-21 10:03:32 +0800

    fix: 修复截图模块依赖

--- packages/docs/fluent-editor/demos/collaborative-editing.vue
+++ packages/docs/fluent-editor/demos/collaborative-editing.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type FluentEditor from '@opentiny/fluent-editor'
+import Html2Canvas from 'html2canvas'
 import katex from 'katex'
 import { onMounted, ref } from 'vue'
 import 'quill-table-up/index.css'
@@ -7,6 +8,8 @@ import 'quill-table-up/table-creator.css'
 import 'katex/dist/katex.min.css'
 
 window.katex = katex
+window.Html2Canvas = Html2Canvas
+
 let editor: FluentEditor
 const editorRef = ref<HTMLElement>()
 
@@ -40,51 +43,60 @@ onMounted(() => {
   Promise.all([
     import('@opentiny/fluent-editor'),
     import('quill-table-up'),
-  ]).then(([
-    { default: FluentEditor, generateTableUp, CollaborationModule },
-    { defaultCustomSelect, TableMenuContextmenu, TableSelection, TableUp },
-  ]) => {
-    if (!editorRef.value) return
+  ]).then(
+    ([
+      { default: FluentEditor, generateTableUp, CollaborationModule },
+      { defaultCustomSelect, TableMenuContextmenu, TableSelection, TableUp },
+    ]) => {
+      if (!editorRef.value) return
 
-    FluentEditor.register({ 'modules/table-up': generateTableUp(TableUp) }, true)
-    FluentEditor.register('modules/collaborative-editing', CollaborationModule, true)
+      FluentEditor.register(
+        { 'modules/table-up': generateTableUp(TableUp) },
+        true,
+      )
+      FluentEditor.register(
+        'modules/collaborative-editing',
+        CollaborationModule,
+        true,
+      )
 
-    editor = new FluentEditor(editorRef.value, {
-      theme: 'snow',
-      modules: {
-        'toolbar': TOOLBAR_CONFIG,
-        'file': true,
-        'emoji': true,
-        'table-up': {
-          customSelect: defaultCustomSelect,
-          selection: TableSelection,
-          selectionOptions: {
-            tableMenu: TableMenuContextmenu,
-          },
-        },
-        'collaborative-editing': {
-          provider: {
-            type: 'websocket',
-            options: {
-              serverUrl: 'wss://120.26.92.145:1234',
-              roomName: ROOM_NAME,
+      editor = new FluentEditor(editorRef.value, {
+        theme: 'snow',
+        modules: {
+          'toolbar': TOOLBAR_CONFIG,
+          'file': true,
+          'emoji': true,
+          'table-up': {
+            customSelect: defaultCustomSelect,
+            selection: TableSelection,
+            selectionOptions: {
+              tableMenu: TableMenuContextmenu,
             },
           },
-          awareness: {
-            state: {
-              color: '#ff6b6b',
+          'collaborative-editing': {
+            provider: {
+              type: 'websocket',
+              options: {
+                serverUrl: 'wss://120.26.92.145:1234',
+                roomName: ROOM_NAME,
+              },
+            },
+            awareness: {
+              state: {
+                color: '#ff6b6b',
+              },
+            },
+            cursors: {
+              hideDelayMs: 300,
+              hideSpeedMs: 300,
+              selectionChangeSource: null,
+              transformOnTextChange: true,
             },
           },
-          cursors: {
-            hideDelayMs: 300,
-            hideSpeedMs: 300,
-            selectionChangeSource: null,
-            transformOnTextChange: true,
-          },
         },
-      },
-    })
-  })
+      })
+    },
+  )
 })
 </script>
 
