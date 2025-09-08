a3dc23cc2b2c5ad296a581f67835ad4cd432787d
yinlin124 <yinlin124@gmail.com>
2025-09-08 14:51:30 +0800

    fix：demo上传图片逻辑

--- packages/docs/fluent-editor/demos/collaborative-editing.vue
+++ packages/docs/fluent-editor/demos/collaborative-editing.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type FluentEditor from '@opentiny/fluent-editor'
+import type { Range } from '@opentiny/fluent-editor'
 import Html2Canvas from 'html2canvas'
 import katex from 'katex'
 import { onMounted, ref } from 'vue'
@@ -67,12 +68,22 @@ onMounted(() => {
         true,
       )
 
+      const Delta = FluentEditor.import('delta')
       editor = new FluentEditor(editorRef.value, {
         theme: 'snow',
         modules: {
           'toolbar': TOOLBAR_CONFIG,
           'file': true,
           'emoji': true,
+          'uploader': {
+            mimetypes: ['image/*'],
+            handler(range: Range, files: File[]) {
+              return files.map((_, i) => i % 2 === 0 ? false : 'https://developer.mozilla.org/static/media/chrome.5e791c51c323fbb93c31.svg')
+            },
+            fail(file: File, range: Range) {
+              this.quill.updateContents(new Delta().retain(range.index).delete(1).insert({ image: 'https://developer.mozilla.org/static/media/edge.741dffaf92fcae238b84.svg' }))
+            },
+          },
           'table-up': {
             customSelect: defaultCustomSelect,
             selection: TableSelection,
