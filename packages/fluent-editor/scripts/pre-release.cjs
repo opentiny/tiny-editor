2a712877b5f40277d0ecf77557526a93b8e2ccb9
Kagol <kagol@sina.com>
2025-11-28 15:25:38 +0800

    fix: fix package.json main entry

--- packages/fluent-editor/scripts/pre-release.cjs
+++ packages/fluent-editor/scripts/pre-release.cjs
@@ -7,6 +7,16 @@ shelljs.cp('-rf', 'package.json', 'dist')
 const targetFile = path.resolve(__dirname, '../dist/package.json')
 const packagejson = require(targetFile)
 
+packagejson.exports = {
+  ".": {
+    "import": "./es/index.es.js",
+    "require": "./lib/index.cjs.js",
+    "types": "./types/index.d.ts"
+  },
+  "./style.css": "./style.css",
+  "./package.json": "./package.json"
+}
+
 // 删除不需要发布到 npm 的字段
 delete packagejson.scripts
 delete packagejson.devDependencies
@@ -33,7 +43,7 @@ if (program.versions) {
 
 function preRelease() {
   shelljs.sed('-i', `"version": "${currentVersion}"`, `"version": "${newVersion}"`, targetFile)
-  shelljs.sed('-i', `"main": "src/index.ts"`, `"main": "lib/index.cjs.js"`, targetFile)
+  shelljs.sed('-i', `"main": "src/index.ts"`, `"main": "es/index.es.js"`, targetFile)
   shelljs.sed('-i', `"module": "src/index.ts"`, `"module": "es/index.es.js"`, targetFile)
   shelljs.cp('-rf', '../../README.md', 'dist')
 }
