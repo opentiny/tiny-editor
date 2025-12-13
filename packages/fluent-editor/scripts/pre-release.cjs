4d0e9c6a21fc94eaec6e6d52d043d67854bddc21
Kagol <kagol@sina.com>
2025-12-13 12:22:28 +0800

    fix: fix pre-release scripts

--- packages/fluent-editor/scripts/pre-release.cjs
+++ packages/fluent-editor/scripts/pre-release.cjs
@@ -24,21 +24,7 @@ delete packagejson.scripts
 delete packagejson.devDependencies
 fs.writeFileSync(targetFile, `${JSON.stringify(packagejson, null, 2)}\n`)
 
-const currentVersion = packagejson.version
-const versionArr = currentVersion.split('.')
-let defaultVersion = currentVersion
-
-if (versionArr.length === 3) {
-  // 普通版本号，比如：3.25.0
-  const [mainVersion, subVersion, phaseVersion] = versionArr
-  defaultVersion = `${mainVersion}.${subVersion}.${+phaseVersion + 1}`
-}
-else {
-  // 带 tag 标记的版本号，比如：4.0.0-alpha.0
-  const [mainVersion, subVersion, tagVersion, phaseVersion] = versionArr
-  defaultVersion = `${mainVersion}.${subVersion}.${tagVersion}.${+phaseVersion + 1}`
-}
-
+let defaultVersion = packagejson.version
 let newVersion = defaultVersion
 
 // 从命令行参数中取版本号
@@ -52,7 +38,7 @@ if (program.versions) {
 }
 
 function preRelease() {
-  shelljs.sed('-i', `"version": "${currentVersion}"`, `"version": "${newVersion}"`, targetFile)
+  shelljs.sed('-i', `"version": "${defaultVersion}"`, `"version": "${newVersion}"`, targetFile)
   shelljs.sed('-i', `"main": "src/index.ts"`, `"main": "es/index.es.js"`, targetFile)
   shelljs.sed('-i', `"module": "src/index.ts"`, `"module": "es/index.es.js"`, targetFile)
   shelljs.sed('-i', `"types": "dist/types/index.d.ts"`, `"types": "types/index.d.ts"`, targetFile)
