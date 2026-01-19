9e74512c0d0b98ab984268589d4486088de779ac
Kagol <kagol@sina.com>
2026-01-19 11:42:59 +0800

    fix: fix hyperlink cannot clicked in read-only mode

--- packages/fluent-editor/src/modules/file/index.ts
+++ packages/fluent-editor/src/modules/file/index.ts
@@ -19,16 +19,19 @@ class FileModule extends Module {
   }
 
   clickEvent(event) {
-    event.preventDefault()
     const target = event.target
     const fileDom = target.closest('a.ql-file-item')
+    const fileBar = target.closest('.ql-file-bar')
+
     if (fileDom) {
+      event.preventDefault()
       if (this.fileBar) {
         this.fileBar.destroy()
       }
       this.fileBar = new FileBar(this.quill, fileDom)
     }
-    else if (this.fileBar && !target.closest('.ql-file-bar')) {
+    else if (this.fileBar && !fileBar) {
+      event.preventDefault()
       this.fileBar.destroy()
       this.fileBar = null
     }
