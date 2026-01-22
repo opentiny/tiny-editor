8141b94b1d43914bb84ab0d94a756e3d6b70cd06
Kagol <kagol@sina.com>
2026-01-22 12:02:52 +0800

    feat: disable shortcut-key by default

---
+++ packages/docs/fluent-editor/demos/shortcut-key.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type FluentEditor from '@opentiny/fluent-editor'
+import { onMounted } from 'vue'
+
+let editor: FluentEditor
+
+onMounted(async () => {
+  // ssr compat, reference: https://vitepress.dev/guide/ssr-compat#importing-in-mounted-hook
+  const { default: FluentEditor } = await import('@opentiny/fluent-editor')
+
+  editor = new FluentEditor('#editor', {
+    theme: 'snow',
+    modules: {
+      'shortcut-key': true,
+    },
+  })
+})
+</script>
+
+<template>
+  <div id="editor" />
+  <br>
+</template>
