fa00a86a19e42118abba3c07ce0205a9fff947a9
wuyiping0628 <1106773985@qq.com>
2026-05-21 20:10:14 -0700

    feat: add e2e

---
+++ packages/docs/fluent-editor/demos/counter.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test'
+
+test('has toolbar and default max character counter', async ({ page }) => {
+  await page.goto('http://localhost:5173/tiny-editor/docs/demo/counter')
+
+  const toolbar = page.locator('.ql-toolbar').first()
+  const editor = page.locator('.ql-editor').first()
+  const counter = page.locator('.ql-counter').first()
+
+  await expect(toolbar).toBeVisible()
+  await expect(editor).toBeVisible()
+  await expect(counter).toBeVisible()
+  await expect(counter).toHaveText('0/500 characters')
+
+  await editor.click()
+  await page.keyboard.type('abc')
+  await expect(counter).toHaveText('3/500 characters')
+})
---
+++ packages/docs/fluent-editor/demos/header-list.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@playwright/test'
+
+test('has toolbar, headings and toggles header list panel', async ({ page }) => {
+  await page.goto('http://localhost:5173/tiny-editor/docs/demo/header-list')
+
+  const block = page.locator('.vp-raw').nth(0)
+  const toolbar = block.locator('.ql-toolbar').first()
+  const editor = block.locator('.ql-editor').first()
+  const headerList = page.locator('.header-list').nth(0)
+  const headerListBtn = toolbar.locator('.ql-header-list')
+
+  await expect(toolbar).toBeVisible({ timeout: 30_000 })
+  await expect(editor).toBeVisible({ timeout: 30_000 })
+  await expect(headerListBtn).toBeVisible()
+  await expect(editor.locator('h1').first()).toHaveText('header 1')
+  await expect(headerList).toHaveClass(/is-hidden/)
+
+  await headerListBtn.click()
+  await expect(headerList).not.toHaveClass(/is-hidden/)
+  await expect(headerList).toContainText('header 1')
+  await expect(headerList).toContainText('header 1.1')
+  await expect(headerList).toContainText('header 2')
+
+  await headerListBtn.click()
+  await expect(headerList).toHaveClass(/is-hidden/)
+})
