90aade5cd04824b1bf8a75b40d3eec16dba06628
wuyiping <42107997+wuyiping0628@users.noreply.github.com>
2026-08-26 09:01:27 +0800

    fix: fix the issue of failed title switching after using the quill-header-list plugin (#475)
    * feat: fix the issue of failed title switching after using the quill-header-list plugin
    
    * feat: fix the issue of failed title switching after using the quill-header-list plugin

--- packages/docs/fluent-editor/demos/header-list.vue
+++ packages/docs/fluent-editor/demos/header-list.vue
@@ -9,13 +9,18 @@ const headerListRef = ref<HTMLElement>()
 
 onMounted(async () => {
   // ssr compat, reference: https://vitepress.dev/guide/ssr-compat#importing-in-mounted-hook
-  const [{ default: FluentEditor, DEFAULT_TOOLBAR }, { default: HeaderList }] = await Promise.all([
+  const [
+    { default: FluentEditor, DEFAULT_TOOLBAR },
+    { default: HeaderList, HeaderWithID },
+  ] = await Promise.all([
     import('@opentiny/fluent-editor'),
     import('quill-header-list'),
   ])
 
   if (!editorRef.value) return
   FluentEditor.register({ 'modules/header-list': HeaderList }, true)
+
+  HeaderWithID.idKey = 'id'
 
   editor = new FluentEditor(editorRef.value, {
     theme: 'snow',
@@ -26,6 +31,9 @@ onMounted(async () => {
           [{ header: [false, 1, 2, 3, 4, 5, 6] }, 'header-list'],
         ],
         handlers: {
+          header(value: string | false) {
+            this.quill.format('header', value === false ? false : Number(value))
+          },
           'header-list': HeaderList.toolbarHandle,
         },
       },
